1f4aeaed767420f4866e6cccacde2cc1f0be9fab
Aung Htet <aung9htet.ah@gmail.com>
2023-03-14 21:59:25 +0000

    Backup

---
+++ src/process_audio.py
@@ -0,0 +1,38 @@
+#!/user/bin/env python3
+import numpy as np
+import os
+import rospy # ROS Python interface
+from std_msgs.msg import Int16MultiArray # ROS message for mics
+
+SAMPLE_COUNT = 1000
+
+class ProcessAudio(object):
+
+    # update the detected words with the messages being published
+    def __init__(self):
+        self.mic_data = np.zeros((0, 4), 'uint16')
+        self.micbuf = np.zeros((0, 4), 'uint16')
+        self.outbuf = None
+        self.startCheck = False
+        self.topic_base_name = "/" + os.getenv("MIRO_ROBOT_NAME")
+        self.subscriber = rospy.Subscriber(
+            self.topic_base_name + "/sensors/mics", Int16MultiArray, self.audio_cb, tcp_nodelay=True
+        )
+
+    def audio_cb(self, msg):
+		# reshape into 4 x 500 array
+        data = np.asarray(msg.data)
+        self.mic_data = np.transpose(data.reshape((4, 500)))
+
+        if not self.micbuf is None:
+
+			# append mic data to store
+            self.micbuf = np.concatenate((self.micbuf, self.mic_data))
+
+			# finished recording?
+            if self.micbuf.shape[0] >= SAMPLE_COUNT:
+
+				# start updating records
+                self.outbuf = self.micbuf[:SAMPLE_COUNT]
+                self.micbuf = self.micbuf[SAMPLE_COUNT:]
+                self.startCheck = True 	# check if micbuf is full since the used audio will be deleted from mic buf
--- src/text_prompt.py
+++ src/text_prompt.py
@@ -1,8 +1,59 @@
-import openai
-import config
+#!/usr/bin/env python2
+import numpy as np
+import wave, struct, rospy
+# import openai, config
 
-openai.api_key = config.API_KEY("aung")
+from process_audio import ProcessAudio
 
-audio_file = open("audio_files/Television.wav", "rb")
-transcript = openai.Audio.transcribe("whisper-1", audio_file)
-print(transcript)
+class TextPrompt():
+
+    def __init__(self):
+        # openai load api key
+        # openai.api_key = config.load_api_key()
+
+        # init node
+        rospy.init_node("text_prompt")
+        self.mic_data = ProcessAudio()
+
+        # state
+        self.outbuf = self.mic_data.outbuf
+        self.buffer_stuff = 0
+        self.startCheck = self.mic_data.startCheck
+    
+    def text_to_speech(self):
+        detected_sound = np.zeroes((0,1), 'uint16')
+
+        # while not rospy.core.is_shutdown():
+        while len(detected_sound) < 100000:
+
+            self.outbuf = self.mic_data.outbuf
+            self.startCheck = self.mic_data.startCheck
+
+            if self.startCheck is True:
+                # get audio from left ear of MiRo
+                detect_sound = np.reshape(self.outbuf[:,[1]], (-1))
+
+                # downsample for playback
+                detected_sound = np.append(detected_sound, detect_sound)
+            
+        outfilename = 'audio_files/testing.wav'
+        file = wave.open(outfilename, 'wb')
+        file.setframerate(20000)
+        file.setsampwidth(2)
+
+        print("writing")
+        file.setnchannels(1)
+        for s in detected_sound:
+            file.writeframes(struct.pack('<h', s))
+        file.close()
+
+if __name__ == "__main__":
+    main = TextPrompt()
+    main.text_to_speech()
+
+
+    # def text_to_speech(self):
+    #     audio_file = open("audio_files/Television.wav", "rb")
+    #     print(audio_file)
+    #     transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    #     print(transcript)
